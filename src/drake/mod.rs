26204792e3e547a2f864d81e3e7d34203cf51cba
Xavientois <joshua.rampersad@hotmail.com>
2021-01-08 14:02:33 -0500

    Add event listeners and tests

--- src/drake/mod.rs
+++ src/drake/mod.rs
@@ -13,6 +13,12 @@ extern "C" {
     #[wasm_bindgen(method)]
     pub fn end(this: &Drake);
 
+    #[wasm_bindgen(method)]
+    pub fn remove(this: &Drake);
+
+    #[wasm_bindgen(method)]
+    pub fn cancel(this: &Drake, revert: bool);
+
     #[wasm_bindgen(method, getter = containers)]
     fn containers_getter_impl(this: &Drake) -> JsValue;
 
@@ -20,8 +26,10 @@ extern "C" {
     fn containers_setter_impl(this: &Drake, val: Box<[JsValue]>);
 
     #[wasm_bindgen(method, js_name = start)]
-    fn start_impl(this: &Drake, item: JsValue) -> bool;
+    fn start_impl(this: &Drake, item: JsValue);
 
+    #[wasm_bindgen(method)]
+    fn on(this: &Drake, event_type: &str, listener: JsValue);
 }
 
 impl Drake {
@@ -40,12 +48,155 @@ impl Drake {
         self.containers_setter_impl(obj_array);
     }
 
+    pub fn add_container<T>(&mut self, obj: T)
+    where
+        T: JsCast,
+    {
+        let mut containers = self.containers();
+        let container_to_add = JsValue::from(&obj);
+        containers.push(container_to_add);
+        self.set_containers(&containers);
+    }
+
     pub fn start<T>(&mut self, item: &T)
     where
         T: JsCast + Clone,
     {
         let item = JsValue::from(item);
         self.start_impl(item);
+    }
+
+    pub fn on_drag<F: 'static>(&mut self, listener: F)
+    where
+        F: FnMut(JsValue, JsValue),
+    {
+        const EVENT_NAME: &str = "drag";
+
+        let closure = Closure::wrap(
+            Box::new(listener) as Box<dyn FnMut(JsValue, JsValue)>
+        );
+
+        let listener = closure.into_js_value();
+
+        self.on(EVENT_NAME, listener);
+    }
+
+    pub fn on_dragend<F: 'static>(&mut self, listener: F)
+    where
+        F: FnMut(JsValue),
+    {
+        const EVENT_NAME: &str = "dragend";
+
+        let closure =
+            Closure::wrap(Box::new(listener) as Box<dyn FnMut(JsValue)>);
+
+        let listener = closure.into_js_value();
+
+        self.on(EVENT_NAME, listener);
+    }
+
+    pub fn on_drop<F: 'static>(&mut self, listener: F)
+    where
+        F: FnMut(JsValue, JsValue, JsValue, JsValue),
+    {
+        const EVENT_NAME: &str = "drop";
+
+        let closure = Closure::wrap(Box::new(listener)
+            as Box<dyn FnMut(JsValue, JsValue, JsValue, JsValue)>);
+
+        let listener = closure.into_js_value();
+
+        self.on(EVENT_NAME, listener);
+    }
+
+    pub fn on_cancel<F: 'static>(&mut self, listener: F)
+    where
+        F: FnMut(JsValue, JsValue, JsValue),
+    {
+        const EVENT_NAME: &str = "cancel";
+
+        let closure = Closure::wrap(
+            Box::new(listener) as Box<dyn FnMut(JsValue, JsValue, JsValue)>
+        );
+
+        let listener = closure.into_js_value();
+
+        self.on(EVENT_NAME, listener);
+    }
+
+    pub fn on_remove<F: 'static>(&mut self, listener: F)
+    where
+        F: FnMut(JsValue, JsValue, JsValue),
+    {
+        const EVENT_NAME: &str = "remove";
+
+        let closure = Closure::wrap(
+            Box::new(listener) as Box<dyn FnMut(JsValue, JsValue, JsValue)>
+        );
+
+        let listener = closure.into_js_value();
+
+        self.on(EVENT_NAME, listener);
+    }
+
+    pub fn on_shadow<F: 'static>(&mut self, listener: F)
+    where
+        F: FnMut(JsValue, JsValue, JsValue),
+    {
+        const EVENT_NAME: &str = "shadow";
+
+        let closure = Closure::wrap(
+            Box::new(listener) as Box<dyn FnMut(JsValue, JsValue, JsValue)>
+        );
+
+        let listener = closure.into_js_value();
+
+        self.on(EVENT_NAME, listener);
+    }
+
+    pub fn on_over<F: 'static>(&mut self, listener: F)
+    where
+        F: FnMut(JsValue, JsValue, JsValue),
+    {
+        const EVENT_NAME: &str = "over";
+
+        let closure = Closure::wrap(
+            Box::new(listener) as Box<dyn FnMut(JsValue, JsValue, JsValue)>
+        );
+
+        let listener = closure.into_js_value();
+
+        self.on(EVENT_NAME, listener);
+    }
+
+    pub fn on_out<F: 'static>(&mut self, listener: F)
+    where
+        F: FnMut(JsValue, JsValue, JsValue),
+    {
+        const EVENT_NAME: &str = "out";
+
+        let closure = Closure::wrap(
+            Box::new(listener) as Box<dyn FnMut(JsValue, JsValue, JsValue)>
+        );
+
+        let listener = closure.into_js_value();
+
+        self.on(EVENT_NAME, listener);
+    }
+
+    pub fn on_cloned<F: 'static>(&mut self, listener: F)
+    where
+        F: FnMut(JsValue, JsValue, JsValue),
+    {
+        const EVENT_NAME: &str = "cloned";
+
+        let closure = Closure::wrap(
+            Box::new(listener) as Box<dyn FnMut(JsValue, JsValue, JsValue)>
+        );
+
+        let listener = closure.into_js_value();
+
+        self.on(EVENT_NAME, listener);
     }
 }
 
--- src/drake/test.rs
+++ src/drake/test.rs
@@ -27,6 +27,26 @@ where
     retval
 }
 
+fn generate_draggable_containers(containers: u32, draggables: u32) -> String {
+    let generate_draggables = |label, count| {
+        (0..count).fold(String::new(), |acc, j| {
+            format!(
+                "{}<div id=\"drag_{}_{}\">Draggable {1}-{2}!</div>",
+                acc, label, j
+            )
+        })
+    };
+
+    (0..containers).fold(String::new(), |acc, i| {
+        format!(
+            "{}<div id=\"cnt_{}\">{}</div>",
+            acc,
+            i,
+            generate_draggables(i, draggables)
+        )
+    })
+}
+
 #[wasm_bindgen_test]
 fn empty_init() {
     let in_containers: Vec<JsValue> = vec![];
@@ -40,10 +60,8 @@ fn empty_init() {
 fn multiple_containers() {
     console_error_panic_hook::set_once();
 
-    for i in 0..8 {
-        let html = (0..i).fold(String::new(), |acc, j| {
-            format!("{}<div id=\"cnt_{}\"></div>", acc, j)
-        });
+    for i in 0..9 {
+        let html = generate_draggable_containers(i, 0);
 
         let test = |element: &Element| {
             let containers = element.children();
@@ -66,4 +84,261 @@ fn multiple_containers() {
 
         run_dom_test(test, &html);
     }
+}
+
+#[wasm_bindgen_test]
+fn set_containers_after() {
+    console_error_panic_hook::set_once();
+
+    let html = generate_draggable_containers(5, 0);
+
+    let test = |element: &Element| {
+        let empty_containers: Vec<JsValue> = vec![];
+
+        let mut drake = dragula(&empty_containers);
+
+        let containers = element.children();
+        let in_containers: Vec<_> = (0..containers.length())
+            .map(|i| containers.item(i))
+            .map(Option::unwrap)
+            .collect();
+
+        drake.set_containers(&in_containers);
+
+        let out_containers: Vec<_> = drake
+            .containers()
+            .iter()
+            .cloned()
+            .map(Element::from)
+            .collect();
+
+        assert_eq!(in_containers, out_containers);
+    };
+
+    run_dom_test(test, &html);
+}
+
+#[wasm_bindgen_test]
+fn add_containers_individually() {
+    console_error_panic_hook::set_once();
+
+    for i in 0..9 {
+        let html = generate_draggable_containers(i, 0);
+
+        let test = |element: &Element| {
+            let empty_containers: Vec<JsValue> = vec![];
+
+            let mut drake = dragula(&empty_containers);
+
+            let containers = element.children();
+            let in_containers: Vec<_> = (0..containers.length())
+                .map(|j| containers.item(j))
+                .map(Option::unwrap)
+                .collect();
+
+            for container in in_containers.iter() {
+                drake.add_container(container.clone());
+            }
+
+            let out_containers: Vec<_> = drake
+                .containers()
+                .iter()
+                .cloned()
+                .map(Element::from)
+                .collect();
+
+            assert_eq!(in_containers, out_containers);
+        };
+
+        run_dom_test(test, &html);
+    }
+}
+
+#[wasm_bindgen_test]
+fn dragging_off_by_default() {
+    let in_containers: Vec<JsValue> = vec![];
+    let drake = dragula(&in_containers);
+    let dragging = drake.dragging();
+
+    assert!(!dragging);
+}
+
+#[wasm_bindgen_test]
+fn start_makes_dragging_true() {
+    console_error_panic_hook::set_once();
+
+    let html = generate_draggable_containers(2, 3);
+
+    let test = |element: &Element| {
+        let containers = element.children();
+        let in_containers: Vec<_> = (0..containers.length())
+            .map(|i| containers.item(i))
+            .map(Option::unwrap)
+            .collect();
+
+        let mut drake = dragula(&in_containers);
+
+        let item = in_containers
+            .first()
+            .unwrap()
+            .first_element_child()
+            .unwrap();
+
+        drake.start(&item);
+
+        let dragging = drake.dragging();
+
+        assert!(dragging);
+    };
+
+    run_dom_test(test, &html);
+}
+
+#[wasm_bindgen_test]
+fn end_makes_dragging_false() {
+    console_error_panic_hook::set_once();
+
+    let html = generate_draggable_containers(2, 3);
+
+    let test = |element: &Element| {
+        let containers = element.children();
+        let in_containers: Vec<_> = (0..containers.length())
+            .map(|i| containers.item(i))
+            .map(Option::unwrap)
+            .collect();
+
+        let mut drake = dragula(&in_containers);
+
+        let item = in_containers
+            .first()
+            .unwrap()
+            .first_element_child()
+            .unwrap();
+
+        drake.start(&item);
+
+        let dragging = drake.dragging();
+        assert!(dragging);
+
+        drake.end();
+
+        let dragging = drake.dragging();
+        assert!(!dragging);
+    };
+
+    run_dom_test(test, &html);
+}
+
+#[wasm_bindgen_test]
+fn remove_removes_element_from_dom() {
+    console_error_panic_hook::set_once();
+
+    let html = generate_draggable_containers(2, 3);
+
+    let test = |element: &Element| {
+        let containers = element.children();
+        let in_containers: Vec<_> = (0..containers.length())
+            .map(|i| containers.item(i))
+            .map(Option::unwrap)
+            .collect();
+
+        let mut drake = dragula(&in_containers);
+
+        let item = in_containers
+            .first()
+            .unwrap()
+            .first_element_child()
+            .unwrap();
+
+        drake.start(&item);
+
+        let dragging = drake.dragging();
+        assert!(dragging);
+
+        drake.remove();
+
+        let parent = item.parent_element();
+        assert!(parent.is_none());
+    };
+
+    run_dom_test(test, &html);
+}
+
+#[wasm_bindgen_test]
+fn remove_makes_dragging_false() {
+    console_error_panic_hook::set_once();
+
+    let html = generate_draggable_containers(2, 3);
+
+    let test = |element: &Element| {
+        let containers = element.children();
+        let in_containers: Vec<_> = (0..containers.length())
+            .map(|i| containers.item(i))
+            .map(Option::unwrap)
+            .collect();
+
+        let mut drake = dragula(&in_containers);
+
+        let item = in_containers
+            .first()
+            .unwrap()
+            .first_element_child()
+            .unwrap();
+
+        drake.start(&item);
+
+        let dragging = drake.dragging();
+        assert!(dragging);
+
+        drake.remove();
+
+        let dragging = drake.dragging();
+        assert_eq!(dragging, false);
+    };
+
+    run_dom_test(test, &html);
+}
+
+#[wasm_bindgen_test]
+fn cancel_triggers_callback() {
+    console_error_panic_hook::set_once();
+
+    const CANCEL_ATTR: &str = "cancelled";
+    const TRUE: &str = "true";
+
+    let html = generate_draggable_containers(2, 3);
+
+    let test = |element: &Element| {
+        let containers = element.children();
+        let in_containers: Vec<_> = (0..containers.length())
+            .map(|i| containers.item(i))
+            .map(Option::unwrap)
+            .collect();
+
+        let mut drake = dragula(&in_containers);
+
+        let item = in_containers
+            .first()
+            .unwrap()
+            .first_element_child()
+            .unwrap();
+
+        let item_ref = item.clone();
+        drake.on_cancel(move |_, _, _| {
+            item_ref
+                .set_attribute(CANCEL_ATTR, TRUE)
+                .expect("Unable to set cancelled attribute");
+        });
+
+        drake.start(&item);
+        drake.cancel(true);
+
+        let was_cancelled = item
+            .get_attribute(CANCEL_ATTR)
+            .expect("Cancel attribute not set");
+
+        assert_eq!(was_cancelled, TRUE);
+    };
+
+    run_dom_test(test, &html);
 }
